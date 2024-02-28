5517185d6fce017d5f2eb205ee51237a0015e494
David Villarreal <wvillar85769@universidadean.edu.co>
2024-02-27 20:58:08 -0500

    task-list-react

--- app/components/todoList/TodoList.jsx
+++ app/components/todoList/TodoList.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import TodoItem from './TodoItem'
+import React from "react";
+import TodoItem from "./TodoItem";
 
-export default function TodoList({todos, handleDeleteTodo, handleUpdateTodo, handleCompleteTodo,}) {
+export default function TodoList({
+  todos,
+  handleDeleteTodo,
+  handleUpdateTodo,
+  handleCompleteTodo,
+}) {
   return (
-    <ul className='list-disc w-5/6'>
-        {todos.map(todo => (
-            <TodoItem
-                key={todo.id}
-                todo={todo}
-                handleDeleteTodo={handleDeleteTodo}
-                handleUpdateTodo={handleUpdateTodo}
-                handleCompleteTodo={handleCompleteTodo} 
-            />
-        ) )}
+    <ul className="list-disc w-5/6">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleDeleteTodo={handleDeleteTodo}
+          handleUpdateTodo={handleUpdateTodo}
+          handleCompleteTodo={handleCompleteTodo}
+        />
+      ))}
     </ul>
-  )
+  );
 }
